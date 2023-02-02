4da3ccc02aa3d33ec0ddb531ff13bf473c3e5f40
Jacob Gilbert <jacobgilb03@gmail.com>
2023-02-02 16:54:52 -0500

    LAST TIME

--- social_cards/migrations/0005_alter_socialcard_tags.py
+++ social_cards/migrations/0005_alter_socialcard_tags.py
@@ -7,7 +7,6 @@ import taggit.managers
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('taggit', '0005_auto_20220424_2025'),
         ('social_cards', '0004_follower_created_follower_followed_follower_user_and_more'),
     ]
 
