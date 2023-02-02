76fac1353868d055ff125dac1ee4620519370eff
Jacob Gilbert <jacobgilb03@gmail.com>
2023-02-02 12:20:06 -0500

    MINOR FIXES

--- social_cards/urls.py
+++ social_cards/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('users/', views.UserView.as_view(), name='profile'),
     path('cards/', views.CardsList.as_view(), name='cards'),
-    path('cards/me/', views.MyCards.as_view(), name='my-cards'),
-    path('cards/<int:card_id>/', views.CardDetail.as_view(), name='card-detail'),
-    path('cards/<int:card_id>/edit', views.CardEdit.as_view(), name='card-edit'),
+    path('cards/me/', views.MyCards.as_view(), name='my_cards'),
+    path('cards/<int:card_id>/', views.CardDetail.as_view(), name='card_detail'),
+    path('cards/<int:card_id>/edit', views.CardEdit.as_view(), name='card_edit'),
     path('search', views.CardSearch.as_view(), name='tags_search'),
 ]
