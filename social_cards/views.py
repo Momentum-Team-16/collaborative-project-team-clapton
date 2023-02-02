67bc2d1220157b7f3cecf2b2902167f79c104145
Jacob Gilbert <jacobgilb03@gmail.com>
2023-02-02 16:25:10 -0500

    Added follower
    Co-authored-by: Jerome Cox jeromecox@hotmail.com

--- social_cards/views.py
+++ social_cards/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404
-from .models import SocialCard, User
-from .serializers import UserSerializer, SocialCardSerializer
-from rest_framework.generics import ListCreateAPIView, RetrieveAPIView, RetrieveUpdateDestroyAPIView, ListAPIView
+from .models import SocialCard, User, Follower
+from .serializers import UserSerializer, SocialCardSerializer, FollowerSerializer
+from rest_framework.generics import ListCreateAPIView, RetrieveAPIView, RetrieveUpdateDestroyAPIView, ListAPIView, RetrieveDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
@@ -62,3 +62,20 @@ class CardSearch(ListAPIView):
         search_term = self.request.query_params.get("tags")
         if search_term is not None:
             return self.queryset.filter(tags__name__in=[search_term])
+
+class FollowerDetail(ListCreateAPIView):
+    queryset = Follower.objects.all()
+    serializer_class = FollowerSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = Follower.objects.filter(user=self.request.user)
+        return queryset
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class FollowerEdit(RetrieveDestroyAPIView):
+    queryset = Follower.objects.all()
+    serializer_class = FollowerSerializer
+    permission_classes = [IsAuthenticated]
