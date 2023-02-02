182312a15aba20482e2525478306c8d26d59e709
Jacob Gilbert <jacobgilb03@gmail.com>
2023-02-02 12:13:44 -0500

    Minor fixes

--- social_cards/urls.py
+++ social_cards/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('users/', views.UserView.as_view(), name='profile'),
     path('cards/', views.CardsList.as_view(), name='cards'),
-    path('cards/me/', views.MyCards.as_view(), name='my-cards'),
-    path('cards/<int:card_id>/', views.CardDetail.as_view(), name='card-detail'),
-    path('cards/<int:card_id>/edit', views.CardEdit.as_view(), name='card-edit'),
+    path('cards/me/', views.MyCards.as_view(), name='my_cards'),
+    path('cards/<int:card_id>/', views.CardDetail.as_view(), name='card_detail'),
+    path('cards/<int:card_id>/edit', views.CardEdit.as_view(), name='card_edit'),
 ]
